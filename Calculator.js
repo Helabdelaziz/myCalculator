2fd60caa440f4dae025e2d916045d8f126477ede
AbdelAziz <ahalaili22@gmail.com>
2025-01-20 23:38:11 +0100

    refactor calc.js

--- Calculator.js
+++ Calculator.js
@@ -1,24 +1,30 @@
-function add(x, y) {
-  // this is commment message
-  return x + y;
-}
-
-function mult(x, y) {
-  return x * y;
-}
-
-function sub(x, y) {
-  return y - x;
-}
-
-function div(x, y) {
-  if (x == 0) {
-    throw EvalError("can't divide on 0");
-  } else {
-    return x / y;
+class Calculator {
+    addition(x, y) {
+      return x + y;
+    }
   }
-}
-
-function abs(res) {
-  return abs(res);
-}
+  function add(x, y) {
+    // this is commment message
+    return y + x;
+  }
+  
+  function sub(x, y) {
+    return x - y;
+  }
+  
+  function mult(x, y) {
+    return x * y;
+  }
+  
+  function sub(x, y) {
+    return y - x;
+  }
+  
+  function div(x, y) {
+    if (x == 0) {
+      throw EvalError("can't divide on 0");
+    } else {
+      return x / y;
+    }
+  }
+  
