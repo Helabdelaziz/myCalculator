fc67ec2f1314db242a7b0a10eaa78ca0264f0541
AbdelAziz <ahalaili22@gmail.com>
2025-01-20 23:03:53 +0100

    Adding division function

--- Calculator.js
+++ Calculator.js
@@ -6,6 +6,14 @@ function mult(x, y) {
   return x * y;
 }
 
+function div(x, y) {
+  if (x == 0) {
+    throw EvalError("can't divide on 0");
+  } else {
+    return x / y;
+  }
+}
+
 function abs(res) {
   return abs(res);
 }
