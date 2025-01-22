20cf456c4b367d4e2f48767817b4381b42535462
AbdelAziz <ahalaili22@gmail.com>
2025-01-22 23:35:38 +0100

    adding typo erro to learn about  git conflicts

--- Calculator.js
+++ Calculator.js
@@ -1,30 +1,29 @@
 class Calculator {
-    addition(x, y) {
-      return x + y;
-    }
+  addition(x, y) {
+    return x + y;
   }
-  function add(x, y) {
-    // this is commment message
-    return y + x;
+}
+function add(x, y) {
+  // this is commment message
+  return y + x;
+}
+
+function sub(x, y) {
+  return x - y;
+}
+
+function mult(x, y) {
+  return x * y;
+}
+
+function sub(x, y) {
+  return y - x;
+}
+
+function div(x, y) {
+  if (x == 0) {
+    throw EvalError("can't divide on 0");
+  } else {
+    return x / y;
   }
-  
-  function sub(x, y) {
-    return x - y;
-  }
-  
-  function mult(x, y) {
-    return x * y;
-  }
-  
-  function sub(x, y) {
-    return y - x;
-  }
-  
-  function div(x, y) {
-    if (x == 0) {
-      throw EvalError("cant divide on 0");
-    } else {
-      return x / y;
-    }
-  }
-  
+}
